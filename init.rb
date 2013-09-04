f18ef3cc41bdf09db0d66c83d13e19e8f081423d
crazyproger <vladimir.rudev@gmail.com>
2013-09-05 00:25:54 +0400

    initial plugin structure

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :time_entries_report do
+  name 'Time Entries Report plugin'
+  author 'crazyproger'
+  description 'This is a plugin for Redmine. It adds custom(or more redmine-like) reports to issue time entries.'
+  version '0.0.1'
+  #url 'http://example.com/path/to/plugin'
+  author_url 'https://github.com/crazyproger'
+end
