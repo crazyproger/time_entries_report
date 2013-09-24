5c88454d21a901b1e586316f875eaf6422054c71
crazyproger <vladimir.rudev@gmail.com>
2013-09-24 22:20:04 +0400

    add 'fixed_version_id' filter

--- app/models/time_entry_extended_query.rb
+++ app/models/time_entry_extended_query.rb
@@ -1,12 +1,27 @@
 class TimeEntryExtendedQuery < TimeEntryQuery
 
+  def initialize_available_filters
+    super
+    if project
+      versions = project.shared_versions.all
+    else
+      versions = Version.visible.find_all_by_sharing('system')
+    end
+    if versions.any?
+      add_available_filter "fixed_version_id",
+                           :type => :list_optional,
+                           :values => versions.sort.collect { |s| ["#{s.project.name} - #{s.name}", s.id.to_s] }
+    end
+  end
+
   def results_scope(sort_clause)
     order_option = [group_by_sort_order, sort_clause].flatten.reject(&:blank?)
 
     TimeEntry.visible.
         where(statement).
         order(order_option).
-        joins(joins_for_order_statement(order_option.join(',')))
+        joins(joins_for_order_statement(order_option.join(','))).
+        includes(:issue => :tracker)
   end
 
   def count(sort_clause)
@@ -15,7 +30,7 @@ class TimeEntryExtendedQuery < TimeEntryQuery
   end
 
   def entries(options = {})
-    scope = results_scope(options.delete(:order)).includes(options.fetch(:include, [:project, :activity, :user, {:issue => :tracker}]))
+    scope = results_scope(options.delete(:order)).includes(options.fetch(:include, [:project, :activity, :user]))
     options.delete(:include)
     grouped = scope.group(:user_id, :issue_id)
     time_entries = grouped.clone.all(options)
@@ -30,6 +45,10 @@ class TimeEntryExtendedQuery < TimeEntryQuery
         entry.hours=times[[entry.user_id, entry.issue_id]]
       end
     end
+  end
+
+  def sql_for_fixed_version_id_field(field, operator, value)
+    '('+sql_for_field(field, operator, value, Issue.table_name, 'fixed_version_id')+')'
   end
 
 end
